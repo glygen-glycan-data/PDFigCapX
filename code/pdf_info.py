7bc2f9b0ec12f3d788e7600a4df6b48ef06091cf
Nicole Mathias <nm1058@georgetown.edu>
2025-11-19 10:02:33 -0500

    using fitz to identify the different components in the pdf

--- code/pdf_info.py
+++ code/pdf_info.py
@@ -6,58 +6,97 @@ filename, height, width, page_no, figure_est_no, layout_bbox, text_mask
 '''
 # from selenium import webdriver
 import json
-import numpy as np
-import matplotlib.pyplot as plt
-import sys
-import cv2
+from pathlib import Path
+import fitz
+
+def pdf_info_from_fitz(pdf_path, page_output_dir):
+    doc = fitz.open(pdf_path)
+    info = {
+        "filename": Path(pdf_path).name,
+        "page_no": doc.page_count,
+        "fig_no_est": 0,
+    }
+    html_info = []
+    output_dir = Path(page_output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    for page_index in range(doc.page_count):
+        page = doc[page_index]
+        page_num = page_index + 1
+
+        # Render PNG so downstream OpenCV steps still work
+        # once the text block coordinates are established (images, drawings, etc are skipped), 
+        # then the image block cooridnates will be established
+        # using code from this repository (opencv is used to detect figure contours, etc) - then the coordinates 
+        # will be used to extract the images from the page pixmap that is saved here.
+        # So in short it is neccessary to save the images of the pdf pages for processing.
+        pix = page.get_pixmap()
+        png_path = output_dir / f"page{page_num}.png"
+        pix.save(str(png_path))
+
+        # Collect text blocks
+        blocks = []
+        for x0, y0, x1, y1, text, block_no, block_type in page.get_text("blocks"):
+            if block_type != 0:  # ignore images/drawings; keep only text blocks
+                continue
+            text = text.strip()
+            if not text:
+                continue
+
+            # scale_x = pix.width / page.rect.width
+            # scale_y = pix.height / page.rect.height
+            
+            bbox = [x0, y0, x1 - x0, y1 - y0]
+            # pixel_bbox = [
+            #     int((bbox[0][0]) * scale_x),
+            #     int((bbox[0][1]) * scale_y),
+            #     int((bbox[0][0] + bbox[0][2]) * scale_x),
+            #     int((bbox[0][1] + bbox[0][3]) * scale_y),
+            # ]
+            blocks.append([bbox, text])
+
+        html_info.append([page_num, blocks, (page.rect.height, page.rect.width)])
+
+    info["page_height"] = doc[0].rect.height
+    info["page_width"] = doc[0].rect.width
+    info["mess_up"] = False
+    info["graph_layout"] = (0, info["page_height"])  # placeholder until you recompute
+
+    json_path = output_dir / f"{info['filename'][:-4]}.json"
+    with open(json_path, "w") as fh:
+        json.dump(html_info, fh)
+
+    doc.close()
+    return info, html_info
 
     # Column width, middle gap, Maximum Figure number will be helpful
 def pdf_info(html_file_path, pdf):
     # Get the pdf info by parsing html
-    
+
+    html_dir = Path(html_file_path)
+    # html_dir.mkdir(parents=True, exist_ok=True)
+
     info = {}
-# obtain file name
-    info['filename'] = pdf
-# obtain page no
-    for_counting = []
-    for page in os.listdir(html_file_path):
-        if page.endswith('.png') & page.startswith('page'):
-            for_counting.append(page)
-    page_no = len(for_counting)
-    for_counting = sorted(for_counting)
-    info['page_no'] = page_no
-# Obtain all html information
-    list_of_htmls = []
     html_info = []
-    html_info_json = html_file_path+'/' + pdf[:-4] + '.json'
-    if os.path.isfile(html_info_json):
-        with open(html_info_json) as json_data:
+
+    json_path = html_dir / f"{pdf[:-4]}.json"
+    png_pages = sorted(p.name for p in html_dir.glob("page*.png"))
+
+    if json_path.exists():
+        with json_path.open() as json_data:
             html_info = json.load(json_data)
-    else:
-        browser = webdriver.Chrome('/usa/pengyuan/Documents/RESEARCH/PDFigCapX/chromedriver/chromedriver')
-        for page_id in range(page_no):
-            page = for_counting[page_id]
-            html_file = 'file://' + html_file_path + '/' + page[:-4] + '.html'
-            list_of_htmls.append(html_file)
-            browser.get(html_file)
-            page_layout = browser.find_element_by_xpath("/html/body/img")
-            img_size = (page_layout.size['height'], page_layout.size['width'])
-            text_elements = browser.find_elements_by_xpath("/html/body/div")
-            text_boxes = []
-            for element in text_elements:
-                text = element.text
-                if len(text) > 0:
-                    text_boxes.append([[element.location['x'], element.location['y'], element.size['width'], element.size['height']], text])
-            html_info.append([int(os.path.basename(html_file)[4:-5]), text_boxes, img_size])
-        browser.quit()
-            #html_info.append(read_each_html(html_file))
-        with open(html_file_path+'/' + pdf[:-4] + '.json', 'w') as outfile:
-            json.dump(html_info, outfile)
-    #multithread = Pool(4)
-    #html_info = multithread.map(read_each_html, list_of_htmls)
-    #multithread.close()
-    #multithread.join()
-# obtain text layout
+
+    if not html_info or not png_pages:
+        info, html_info = pdf_info_from_fitz(pdf, str(html_dir))
+        png_pages = [f"page{page_num}.png" for page_num in range(1, info["page_no"] + 1)]
+
+    if not info:
+        info = {"filename": pdf}
+
+    info['filename'] = pdf
+    page_no = len(png_pages) if png_pages else info.get('page_no', len(html_info))
+    info['page_no'] = page_no
+
     row_width = []
     row_height = []
     column_no = 1
@@ -65,6 +104,11 @@ def pdf_info(html_file_path, pdf):
     left_point = []
     top_point = []
     right_point = []
+
+    if html_info:
+        first_page_size = html_info[0][2]
+        info.setdefault('page_height', first_page_size[0])
+        info.setdefault('page_width', first_page_size[1])
 
     if page_no > 3:
         list_to_check = range(2, page_no)
@@ -77,6 +121,7 @@ def pdf_info(html_file_path, pdf):
             info['page_height'] = each_page_html[2][0]
             info['page_width'] = each_page_html[2][1]
             for element in each_page_html[1]:
+                # element[1] - is the text data - is no. of words > 30 --> collect the dimensions of the text block (w, h, etc)
                 if len(element[1]) > 30:
                     row_width.append(element[0][2])
                     row_height.append(element[0][3])
@@ -92,7 +137,7 @@ def pdf_info(html_file_path, pdf):
     info['row_height'] = height_row[0][0]
     info['row_width'] = width_row[0][0]
     info['text_layout'] = (max(0, min(top_point)),
-                           min(info['page_height'],max(top_point)))
+                           min(info['page_height'],max(top_point)))     # layout of columns - 1 or 2 columns page etc 
 
     # Compute column no and position for each column
     i = 0
@@ -264,19 +309,19 @@ def pdf_info(html_file_path, pdf):
 
 
 
-def read_each_html(x):
-    #browser = webdriver.Chrome('/home/pengyuan/chromedriver')
-    #browser = webdriver.Chrome('/usa/pengyuan/Documents/RESEARCH/PDFigCapX/chromedriver/chromedriver')
-    #browser.implicitly_wait(2)
-    browser.get(x)
-    page_layout = browser.find_element_by_xpath("/html/body/img")
-    img_size = (page_layout.size['height'], page_layout.size['width'])
-    text_elements = browser.find_elements_by_xpath("/html/body/div")
-    text_boxes = []
-    for element in text_elements:
-        text = element.text
-        if len(text) > 0:
-            text_boxes.append([[element.location['x'], element.location['y'], element.size['width'], element.size['height']], text])
+# def read_each_html(x):
+#     #browser = webdriver.Chrome('/home/pengyuan/chromedriver')
+#     #browser = webdriver.Chrome('/usa/pengyuan/Documents/RESEARCH/PDFigCapX/chromedriver/chromedriver')
+#     #browser.implicitly_wait(2)
+#     browser.get(x)
+#     page_layout = browser.find_element_by_xpath("/html/body/img")
+#     img_size = (page_layout.size['height'], page_layout.size['width'])
+#     text_elements = browser.find_elements_by_xpath("/html/body/div")
+#     text_boxes = []
+#     for element in text_elements:
+#         text = element.text
+#         if len(text) > 0:
+#             text_boxes.append([[element.location['x'], element.location['y'], element.size['width'], element.size['height']], text])
 
-    browser.quit()
-    return int(os.path.basename(x)[4:-5]), text_boxes, img_size
+#     browser.quit()
+#     return int(os.path.basename(x)[4:-5]), text_boxes, img_size
